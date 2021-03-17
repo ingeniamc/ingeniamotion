b911817cf6310f652c324546efdc1584a81b28f2
Javier Portillo <javier.portillo@novanta.com>
2021-03-17 10:34:37 +0100

    INGM-29 Remove magic numbers

--- examples/velocity_torque_ramp.py
+++ examples/velocity_torque_ramp.py
@@ -4,6 +4,9 @@ import argparse
 from ingeniamotion import MotionController
 from ingenialink.exceptions import ILError
 
+# Set your motor torque constant
+TORQUE_CONSTANT = 0.0376
+
 
 def setup_command():
     parser = argparse.ArgumentParser(description='Run feedback test')
@@ -15,6 +18,14 @@ def setup_command():
 
 
 def velocity_ramp(final_velocity, acceleration, mc):
+    """
+    Creates a velocity ramp from current velocity to ``final_velocity`` with ``acceleration`` as a ramp slope.
+
+    Args:
+        final_velocity: target velocity in rev/s.
+        acceleration: acceleration in rev/s^2.
+        mc: MotionController instance.
+    """
     done = False
     mc.configuration.set_max_acceleration(acceleration)
     mc.motion.set_velocity(final_velocity)
@@ -26,9 +37,18 @@ def velocity_ramp(final_velocity, acceleration, mc):
             pass
 
 
-def torque_ramp(final_torque, rotatum, mc):
+def torque_ramp(final_torque, rotatum, torque_constant, mc):
+    """
+    Creates a torque ramp from 0 to ``final_torque`` with ``rotatum`` as a ramp slope.
+
+    Args:
+        final_torque: target torque in Nm.
+        rotatum: torque derivative in Nm/s.
+        torque_constant: motor torque constant.
+        mc: MotionController instance.
+    """
     torque = 0
-    torque_constant = 0.0376
+    torque_constant = torque_constant
     init_time = time.time()
     while torque < final_torque:
         try:
@@ -40,20 +60,25 @@ def torque_ramp(final_torque, rotatum, mc):
 
 
 def velocity_demo(mc):
+    target_velocity = [16.667, 66.667, 0]
+    acceleration = [0.1667, 0.8333, 20]
+    wait_in_seconds = 40
     mc.motion.set_operation_mode(mc.motion.OperationMode.PROFILE_VELOCITY)
     mc.configuration.set_max_velocity(70)
     mc.motion.motor_enable()
-    velocity_ramp(16.667, 0.1667, mc)
-    velocity_ramp(66.667, 0.8333, mc)
-    time.sleep(40)
-    velocity_ramp(0, 20, mc)
+    velocity_ramp(target_velocity[0], acceleration[0], mc)
+    velocity_ramp(target_velocity[1], acceleration[1], mc)
+    time.sleep(wait_in_seconds)
+    velocity_ramp(target_velocity[2], acceleration[2], mc)
     mc.motion.motor_disable()
 
 
 def torque_demo(mc):
+    target_torque = 0.706
+    rotatum = 0.0035
     mc.motion.set_operation_mode(mc.motion.OperationMode.CURRENT)
     mc.motion.motor_enable()
-    torque_ramp(0.706, 0.0035, mc)
+    torque_ramp(target_torque, rotatum, TORQUE_CONSTANT, mc)
     mc.motion.set_current_quadrature(0)
     mc.motion.motor_disable()
 
