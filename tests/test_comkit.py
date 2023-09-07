33cef632cf726266a7a81e33dcc80699fd2040e7
Martin.Acosta <martin.acosta@novanta.com>
2023-09-07 16:22:11 +0200

    INGM-321 Fix test

--- tests/test_comkit.py
+++ tests/test_comkit.py
@@ -1,3 +1,6 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
 import pytest
 
 from ingeniamotion.comkit import create_comkit_dictionary, get_tree_root
@@ -5,9 +8,9 @@ from ingeniamotion.comkit import create_comkit_dictionary, get_tree_root
 
 @pytest.mark.smoke
 def test_create_comkit_dictionary():
-    coco_path = "test//resources//com-kit.xdf"
-    moco_path = "test//resources//core.xdf"
-    reference_dict = "test//resources//com-kit-core.xdf"
+    coco_path = "./tests/resources/com-kit.xdf"
+    moco_path = "./tests/resources//core.xdf"
+    reference_dict = "./tests/resources/com-kit-core.xdf"
     comkit_dict = create_comkit_dictionary(coco_path, moco_path)
     comkit_root = get_tree_root(comkit_dict)
     reference_root = get_tree_root(reference_dict)
