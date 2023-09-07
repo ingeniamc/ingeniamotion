7620a2c30aad15913241d55c16deb59ccc2059fb
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2023-09-07 16:21:29 +0200

    INGM-289: mypy wizard_tests/stoppable.

--- ingeniamotion/wizard_tests/stoppable.py
+++ ingeniamotion/wizard_tests/stoppable.py
@@ -1,6 +1,8 @@
 from time import time
 from functools import wraps
+from typing import TypeVar, Callable, Any
 
+F = TypeVar('F', bound=Callable[..., Any])
 
 class StopException(Exception):
     """Stop exception."""
@@ -11,25 +13,25 @@ class Stoppable:
     is_stopped = False
 
     @staticmethod
-    def stoppable(fun):
+    def stoppable(fun: Callable[..., Any]) -> Callable[..., Any]:
         @wraps(fun)
-        def wrapper(self, *args, **kwargs):
+        def wrapper(self, *args, **kwargs): #type: ignore
             self.check_stop()
             return fun(self, *args, **kwargs)
 
         return wrapper
 
-    def reset_stop(self):
+    def reset_stop(self) -> None:
         self.is_stopped = False
 
-    def stop(self):
+    def stop(self) -> None:
         self.is_stopped = True
 
-    def check_stop(self):
+    def check_stop(self) -> None:
         if self.is_stopped:
             raise StopException
 
-    def stoppable_sleep(self, timeout):
+    def stoppable_sleep(self, timeout: float) -> None:
         init_time = time()
         while init_time + timeout > time():
             self.check_stop()
