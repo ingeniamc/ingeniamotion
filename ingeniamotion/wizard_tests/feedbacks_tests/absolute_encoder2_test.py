d5ff5c6a27a7859fc7dd60064dfc9a8dfca955bc
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2023-09-08 15:30:23 +0200

    INGM-289: mypy absolute_encoder2_test.

--- ingeniamotion/wizard_tests/feedbacks_tests/absolute_encoder2_test.py
+++ ingeniamotion/wizard_tests/feedbacks_tests/absolute_encoder2_test.py
@@ -1,3 +1,4 @@
+from ingeniamotion import MotionController
 from ingeniamotion.wizard_tests.feedbacks_tests.feedback_test import Feedbacks
 from ingeniamotion.wizard_tests.base_test import BaseTest
 from ingeniamotion.enums import SensorType
@@ -10,12 +11,12 @@ class AbsoluteEncoder2Test(Feedbacks):
 
     SENSOR_TYPE_FEEDBACK_TEST = SensorType.BISSC2
 
-    def __init__(self, mc, servo, axis):
+    def __init__(self, mc: MotionController, servo:str, axis:int):
         super().__init__(mc, servo, axis)
-        self.backup_registers_names += self.BACKUP_REGISTERS_BISSC2
+        self.backup_registers_names.append(*self.BACKUP_REGISTERS_BISSC2)
 
     @BaseTest.stoppable
-    def feedback_setting(self):
+    def feedback_setting(self) -> None:
         super().feedback_setting()
         self.mc.configuration.set_auxiliar_feedback(
             SensorType.ABS1, servo=self.servo, axis=self.axis
