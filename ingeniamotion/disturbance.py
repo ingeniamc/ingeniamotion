8afd367a43d5d387aa2664d8ed94fb8772be023b
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2023-09-19 17:41:45 +0200

    INGM-289: disturbance.py

--- ingeniamotion/disturbance.py
+++ ingeniamotion/disturbance.py
@@ -2,20 +2,27 @@ import ingenialogger
 from numpy import ndarray
 from functools import wraps
 from collections.abc import Iterable
-from typing import Union, TYPE_CHECKING, List
+from typing import Callable, Union, TYPE_CHECKING
+from ingenialink.enums.register import REG_DTYPE
 
-from ingeniamotion.enums import MonitoringVersion, REG_DTYPE
-from .metaclass import DEFAULT_SERVO, DEFAULT_AXIS
-from .exceptions import IMDisturbanceError, IMStatusWordError
+from ingeniamotion.enums import MonitoringVersion
+from ingeniamotion.metaclass import DEFAULT_SERVO, DEFAULT_AXIS
+from ingeniamotion.exceptions import IMDisturbanceError, IMException, IMStatusWordError
 
 
 if TYPE_CHECKING:
     from ingeniamotion.motion_controller import MotionController
 
+# Constants for typing
+TYPE_MAPPED_REGISTERS_ALL = dict[str, Union[str, int, list[float]]]
+TYPE_MAPPED_REGISTERS_NAME_AXIS = dict[str, Union[str, int]]
+TYPE_MAPPED_REGISTERS_DATA = dict[str, list[Union[int, float]]]
+TYPE_MAPPED_REGISTERS_DATA_NO_KEY = list[Union[int, float]]
 
-def check_disturbance_disabled(func):
+
+def check_disturbance_disabled(func: Callable[..., Union[int, float, None]]) -> Callable[..., Union[int, float, None]]:
     @wraps(func)
-    def wrapper(self, *args, **kwargs):
+    def wrapper(self, *args, **kwargs): # type: ignore
         disturbance_enabled = self.mc.capture.is_disturbance_enabled(
             servo=self.servo, version=self._version
         )
@@ -56,11 +63,11 @@ class Disturbance:
         REG_DTYPE.FLOAT: 4,
     }
 
-    def __init__(self, mc: "MotionController", servo: str = DEFAULT_SERVO):
+    def __init__(self, mc: "MotionController", servo: str = DEFAULT_SERVO) -> None:
         super().__init__()
         self.mc = mc
         self.servo = servo
-        self.mapped_registers = []
+        self.mapped_registers: list[TYPE_MAPPED_REGISTERS_NAME_AXIS] = []
         self.sampling_freq = None
         self._version = mc.capture._check_version(servo)
         self.logger = ingenialogger.get_logger(__name__, drive=mc.servo_name(servo))
@@ -101,7 +108,7 @@ class Disturbance:
         return 1 / self.sampling_freq
 
     @check_disturbance_disabled
-    def map_registers(self, registers: Union[dict, List[dict]]) -> int:
+    def map_registers(self, registers: Union[TYPE_MAPPED_REGISTERS_NAME_AXIS, list[TYPE_MAPPED_REGISTERS_NAME_AXIS]]) -> float:
         """Map registers to Disturbance. Disturbance must be disabled.
 
         Args:
@@ -133,7 +140,11 @@ class Disturbance:
         total_sample_size = 0
         for ch_idx, channel in enumerate(registers):
             subnode = channel.get("axis", DEFAULT_AXIS)
+            if not isinstance(subnode, int):
+                raise IMException("Subnode value has to be an integer")
             register = channel["name"]
+            if not isinstance(register, str):
+                raise IMException("Register key has to be a string")
             register_obj = self.mc.info.register_info(register, subnode, servo=self.servo)
             dtype = register_obj.dtype
             cyclic = register_obj.cyclic
@@ -155,19 +166,24 @@ class Disturbance:
         return self.max_sample_number / total_sample_size
 
     @staticmethod
-    def __registers_data_adapter(registers_data):
+    def __registers_data_adapter(registers_data: Union[list[Union[int, float, ndarray, TYPE_MAPPED_REGISTERS_DATA_NO_KEY]], ndarray]) -> list[TYPE_MAPPED_REGISTERS_DATA_NO_KEY]:
         if isinstance(registers_data, ndarray):
             registers_data = registers_data.tolist()
-        if isinstance(registers_data, Iterable) and not isinstance(registers_data[0], Iterable):
             return [registers_data]
-        if isinstance(registers_data, Iterable):
+        elif isinstance(registers_data, list) and all(isinstance(data, list) for data in registers_data):
+            return registers_data
+        elif isinstance(registers_data, list) and all(isinstance(data, (int, float)) for data in registers_data):
+            return [registers_data]
+        elif isinstance(registers_data, list) and all(isinstance(data, ndarray) for data in registers_data):
             for i, x in enumerate(registers_data):
                 if isinstance(x, ndarray):
                     registers_data[i] = x.tolist()
-        return registers_data
+            return registers_data
+        else:
+            raise IMException("Registers data adapter doesn't have the correct type for its input argument")
 
     @check_disturbance_disabled
-    def write_disturbance_data(self, registers_data: List[Union[list, float, int]]) -> None:
+    def write_disturbance_data(self, registers_data: Union[list[Union[int, float, ndarray, TYPE_MAPPED_REGISTERS_DATA_NO_KEY]], ndarray]) -> None:
         """Write data in mapped registers. Disturbance must be disabled.
 
         Args:
@@ -190,7 +206,7 @@ class Disturbance:
         dtype_list = [REG_DTYPE(x["dtype"]) for x in self.mapped_registers]
         drive.disturbance_write_data(idx_list, dtype_list, registers_data)
 
-    def map_registers_and_write_data(self, registers: Union[dict, List[dict]]) -> None:
+    def map_registers_and_write_data(self, registers: Union[TYPE_MAPPED_REGISTERS_ALL, list[TYPE_MAPPED_REGISTERS_ALL]]) -> None:
         """Map registers to Disturbance and write data. Disturbance must be disabled.
 
         Args:
@@ -224,7 +240,7 @@ class Disturbance:
         self.map_registers(registers_keys)
         self.write_disturbance_data(registers_data)
 
-    def __check_buffer_size_is_enough(self, registers: List[Union[list, float, int]]) -> None:
+    def __check_buffer_size_is_enough(self, registers: list[TYPE_MAPPED_REGISTERS_DATA_NO_KEY]) -> None:
         total_buffer_size = 0
         for ch_idx, data in enumerate(registers):
             dtype = self.mapped_registers[ch_idx]["dtype"]
