085ac16a673db57ce3da03af0f06e6674398a812
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2023-09-20 11:15:07 +0200

    INGM-289: monitoring v1 and v3

--- ingeniamotion/monitoring/monitoring_v1.py
+++ ingeniamotion/monitoring/monitoring_v1.py
@@ -1,10 +1,12 @@
+from typing import Optional, Union
 import ingenialogger
 
 from ingeniamotion.metaclass import DEFAULT_SERVO
-from ingeniamotion.exceptions import IMStatusWordError
+from ingeniamotion.exceptions import IMException, IMStatusWordError
 from ingeniamotion.enums import MonitoringVersion, MonitoringSoCType, MonitoringSoCConfig
+from ingeniamotion.motion_controller import MotionController
 
-from .base_monitoring import Monitoring, check_monitoring_disabled
+from ingeniamotion.monitoring.base_monitoring import Monitoring, check_monitoring_disabled
 
 
 class MonitoringV1(Monitoring):
@@ -21,7 +23,7 @@ class MonitoringV1(Monitoring):
         MonitoringSoCConfig.TRIGGER_CONFIG_FALLING: "MON_CFG_FALLING_CONDITION",
     }
 
-    def __init__(self, mc, servo=DEFAULT_SERVO):
+    def __init__(self, mc: MotionController, servo:str = DEFAULT_SERVO) -> None:
         super().__init__(mc, servo)
         self._version = mc.capture._check_version(servo)
         self.logger = ingenialogger.get_logger(__name__, drive=mc.servo_name(servo))
@@ -32,7 +34,7 @@ class MonitoringV1(Monitoring):
                 "MCB could not be synchronized. Motor is enabled.", drive=mc.servo_name(servo)
             )
 
-    def __get_old_soc_type(self, edge_condition):
+    def __get_old_soc_type(self, edge_condition: MonitoringSoCConfig) -> int:
         if edge_condition == MonitoringSoCConfig.TRIGGER_CONFIG_RISING:
             return self.TRIGGER_CYCLIC_RISING_EDGE
         if edge_condition == MonitoringSoCConfig.TRIGGER_CONFIG_FALLING:
@@ -41,8 +43,8 @@ class MonitoringV1(Monitoring):
 
     @check_monitoring_disabled
     def set_trigger(
-        self, trigger_mode, edge_condition=None, trigger_signal=None, trigger_value=None
-    ):
+        self, trigger_mode: Union[MonitoringSoCType, int], edge_condition: Optional[MonitoringSoCConfig]=None, trigger_signal: Optional[dict[str, str]]=None, trigger_value: Union[int, float, None]=None
+    ) -> None:
         self.rearm_monitoring()
         if trigger_mode == MonitoringSoCType.TRIGGER_EVENT_EDGE:
             if trigger_signal is None or trigger_value is None:
@@ -58,7 +60,7 @@ class MonitoringV1(Monitoring):
             self.MONITOR_START_CONDITION_TYPE_REGISTER, trigger_mode, servo=self.servo, axis=0
         )
 
-    def __rising_or_falling_edge_trigger(self, edge_condition, index_reg, level_edge):
+    def __rising_or_falling_edge_trigger(self, edge_condition: MonitoringSoCConfig, index_reg: int, level_edge: int) -> None:
         self.mc.communication.set_register(
             self.MONITORING_INDEX_CHECKER_REGISTER, index_reg, servo=self.servo, axis=0
         )
@@ -67,7 +69,7 @@ class MonitoringV1(Monitoring):
         )
 
     @check_monitoring_disabled
-    def configure_number_samples(self, total_num_samples, trigger_delay_samples):
+    def configure_number_samples(self, total_num_samples: int, trigger_delay_samples: int) -> None:
         if trigger_delay_samples > total_num_samples:
             raise ValueError("trigger_delay_samples should be less than total_num_samples")
         if trigger_delay_samples < 0:
@@ -98,7 +100,7 @@ class MonitoringV1(Monitoring):
         self.samples_number = total_num_samples
         self.trigger_delay_samples = trigger_delay_samples
 
-    def _check_monitoring_is_ready(self):
+    def _check_monitoring_is_ready(self) -> tuple[bool, Optional[str]]:
         is_enabled = self.mc.capture.is_monitoring_enabled(self.servo)
         result_text = None
         trigger_repetitions = self.mc.communication.get_register(
@@ -115,21 +117,23 @@ class MonitoringV1(Monitoring):
             )
         return is_ready, result_text
 
-    def _check_data_is_ready(self):
+    def _check_data_is_ready(self) -> bool:
         monit_nmb_blocks = self.mc.communication.get_register(
             self.MONITORING_ACTUAL_NUMBER_SAMPLES_REGISTER, servo=self.servo, axis=0
         )
+        if not isinstance(monit_nmb_blocks, int):
+            raise IMException("Actual number of monitoring samples value has to be an integer")
         data_is_ready = monit_nmb_blocks > 0
         if self._version == MonitoringVersion.MONITORING_V2:
             data_is_ready &= self.mc.capture.is_frame_available(self.servo, version=self._version)
         return data_is_ready
 
-    def rearm_monitoring(self):
+    def rearm_monitoring(self) -> None:
         self.mc.communication.set_register(
             self.MONITORING_NUMBER_TRIGGER_REPETITIONS_REGISTER, 1, servo=self.servo, axis=0
         )
 
-    def _check_buffer_size_is_enough(self, total_samples, trigger_delay_samples, registers):
+    def _check_buffer_size_is_enough(self, total_samples: int, trigger_delay_samples: int, registers: list[dict[str, str]]) -> None:
         n_sample = max(total_samples - trigger_delay_samples, trigger_delay_samples)
         max_size = self.max_sample_number // 2
         self._check_samples_and_max_size(n_sample, max_size, registers)
