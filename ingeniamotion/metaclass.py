4f5420f965e24e792c307386a91c1cd6582f6e11
Javier Portillo Medina <javierportillo93@gmail.com>
2023-10-04 15:43:51 +0200

    INGM-290 Fix bug in check_motor_disabled decorator

--- ingeniamotion/metaclass.py
+++ ingeniamotion/metaclass.py
@@ -16,6 +16,7 @@ class MCMetaClass(type):
     """
 
     SERVO_ARG_NAME = "servo"
+    AXIS_ARG_NAME = "axis"
 
     def __new__(mcs, name, bases, local):
         """If a function has argument named servo,
@@ -57,8 +58,17 @@ class MCMetaClass(type):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
             mc = self.mc
-            servo = kwargs.get("servo", DEFAULT_SERVO)
-            axis = kwargs.get("axis", DEFAULT_AXIS)
+            func_args = inspect.getfullargspec(func).args
+            servo_index = func_args.index(mcs.SERVO_ARG_NAME)
+            axis_index = func_args.index(mcs.AXIS_ARG_NAME)
+            if len(args) < servo_index:
+                servo = kwargs.get(mcs.SERVO_ARG_NAME, DEFAULT_SERVO)
+            else:
+                servo = args[servo_index - 1]
+            if len(args) < axis_index:
+                axis = kwargs.get(mcs.AXIS_ARG_NAME, DEFAULT_AXIS)
+            else:
+                axis = args[axis_index - 1]
             if mc.configuration.is_motor_enabled(servo=servo, axis=axis):
                 raise IMStatusWordError("Motor is enabled")
             return func(self, *args, **kwargs)
