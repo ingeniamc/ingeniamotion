2355c491c4d57082617f056dd6955d3d505ce2fb
Martin.Acosta <martin.acosta@novanta.com>
2023-11-27 11:22:38 +0100

    INGM-359 Add test_poller_example

--- examples/poller_example.py
+++ examples/poller_example.py
@@ -1,39 +1,63 @@
+import argparse
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 from ingeniamotion import MotionController
 
-mc = MotionController()
-mc.communication.connect_servo_eoe("192.168.2.22", "cap-net_0.5.0.xdf")
 
-# List of registers
-registers = [
-    {
-        "name": "CL_POS_FBK_VALUE",  # Register name
-        "axis": 1  # Register axis
-    },
-]
-# Create Poller with our registers
-poller = mc.capture.create_poller(registers)
+def main(args):
+    mc = MotionController()
+    mc.communication.connect_servo_eoe(args.ip, args.dictionary_path)
 
-# PLOT DATA
-fig, ax = plt.subplots()
-x_values = []
-y_values = []
-line, = ax.plot(x_values, y_values)
+    # List of registers
+    registers = [
+        {
+            "name": "CL_POS_FBK_VALUE",  # Register name
+            "axis": 1  # Register axis
+        },
+    ]
+    # Create Poller with our registers
+    poller = mc.capture.create_poller(registers)
 
-def animate(i):
-    timestamp, registers_values, _ = poller.data  # Read Poller data
+    # PLOT DATA
+    fig, ax = plt.subplots()
     global x_values, y_values
-    x_values += timestamp
-    y_values += registers_values[0]
-    line.set_data(x_values, y_values)
-    ax.relim()
-    ax.autoscale_view()
-    return line,
+    x_values = []
+    y_values = []
+    line, = ax.plot(x_values, y_values)
 
-ani = animation.FuncAnimation(
-    fig, animate, interval=100)
-plt.autoscale()
-plt.show()
-mc.communication.disconnect()
+    def animate(i):
+        timestamp, registers_values, _ = poller.data  # Read Poller data
+        global x_values, y_values
+        x_values += timestamp
+        y_values += registers_values[0]
+        line.set_data(x_values, y_values)
+        ax.relim()
+        ax.autoscale_view()
+        return line,
+
+    ani = animation.FuncAnimation(
+        fig, animate, interval=100)
+    plt.autoscale()
+    if args.close:
+        plt.show(block=False)
+        plt.pause(5)
+        plt.close()
+    else:
+        plt.show()
+    poller.stop()
+    mc.communication.disconnect()
+
+
+def setup_command():
+    parser = argparse.ArgumentParser(description='Disturbance example')
+    parser.add_argument('--dictionary_path', help='Path to drive dictionary', required=True)
+    parser.add_argument('--ip', help='Drive IP address', required=True)
+    parser.add_argument('--close', help='Close plot after 5 seconds', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = setup_command()
+    main(args)
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -36,3 +36,14 @@ def test_set_get_register_example(read_config, script_runner):
     dictionary = read_config["dictionary"]
     result = script_runner.run(script_path, f"--ip={ip_address}", f"--dictionary_path={dictionary}")
     assert result.returncode == 0
+
+
+@pytest.mark.eoe
+def test_poller_example(read_config, script_runner):
+    script_path = "examples/poller_example.py"
+    ip_address = read_config["ip"]
+    dictionary = read_config["dictionary"]
+    result = script_runner.run(
+        script_path, f"--ip={ip_address}", f"--dictionary_path={dictionary}", "--close"
+    )
+    assert result.returncode == 0
