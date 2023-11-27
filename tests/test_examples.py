e44c46de997f122a9e71dd512ad5dddd8f852fe5
Martin.Acosta <martin.acosta@novanta.com>
2023-11-27 14:22:10 +0100

    INGM-359 Add test_monitoring_example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -83,3 +83,14 @@ def test_velocity_torque_ramp_example(read_config, script_runner, mocker, mode):
     mocker.patch.object(MotionController, "motion", MockMotion)
     result = script_runner.run(script_path, mode, dictionary, f"-ip={ip_address}", "-no_wait")
     assert result.returncode == 0
+
+
+@pytest.mark.eoe
+def test_monitoring_example(read_config, script_runner):
+    script_path = "examples/monitoring_example.py"
+    ip_address = read_config["ip"]
+    dictionary = read_config["dictionary"]
+    result = script_runner.run(
+        script_path, f"--ip={ip_address}", f"--dictionary_path={dictionary}", "--close"
+    )
+    assert result.returncode == 0
