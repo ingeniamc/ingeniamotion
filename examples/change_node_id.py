277f1a957ae7a5d300f4f61932df687a0548aa6a
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2024-04-05 12:12:39 +0200

    INGM-406: node_id is an optional input argument.

--- examples/change_node_id.py
+++ examples/change_node_id.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from ingenialink import CAN_BAUDRATE, CAN_DEVICE
 
@@ -10,7 +10,7 @@ import ingenialogger
 logger = ingenialogger.get_logger(__name__)
 
 
-def establish_canopen_communication(mc: MotionController, device: CAN_DEVICE, channel: int, node_id: int, baudrate: CAN_BAUDRATE, dictionary_path: str) -> None:
+def establish_canopen_communication(mc: MotionController, device: CAN_DEVICE, channel: int, baudrate: CAN_BAUDRATE, dictionary_path: str, node_id: Optional[int]) -> None:
     print("Finding the available nodes...")
     node_id_list = mc.communication.scan_servos_canopen(
         device,
@@ -42,10 +42,10 @@ def establish_canopen_communication(mc: MotionController, device: CAN_DEVICE, ch
         raise (f"The drive can be connected with {node_to_connect} as a node ID.")
 
 
-def change_node_id(device: CAN_DEVICE, channel: int, node_id: int, baudrate: CAN_BAUDRATE, dictionary_path: str, new_node_id: int) -> None:
+def change_node_id(device: CAN_DEVICE, channel: int, baudrate: CAN_BAUDRATE, dictionary_path: str, new_node_id: int, node_id: Optional[int] = None) -> None:
     mc = MotionController()
     try:
-        establish_canopen_communication(mc, device, channel, node_id, baudrate, dictionary_path)
+        establish_canopen_communication(mc, device, channel, baudrate, dictionary_path, node_id)
     except IMException as e:
         print(e)
         return
@@ -64,7 +64,7 @@ def change_node_id(device: CAN_DEVICE, channel: int, node_id: int, baudrate: CAN
 
     print("Starts to establish a communication again.")
     try:
-        establish_canopen_communication(mc, device, channel, node_id, baudrate, dictionary_path)
+        establish_canopen_communication(mc, device, channel, baudrate, dictionary_path, node_id)
     except IMException as e:
         print(e)
         return
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     baudrate = CAN_BAUDRATE.Baudrate_1M,
     dictionary_path = "\\\\awe-srv-max-prd\\distext\\products\\EVE-NET\\firmware\\2.5.1\\eve-net-c_can_2.5.1.xdf",
 
-    change_node_id(device, channel, node_id, baudrate, dictionary_path, 20)
+    change_node_id(device, channel, baudrate, dictionary_path, 20, node_id)
