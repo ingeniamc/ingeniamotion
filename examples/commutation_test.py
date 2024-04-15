4fcea0013c7438f4521ade11f6f591467a10ea67
Alberto Zubiri Carrizosa <alberto.zubiri@novanta.com>
2024-04-15 11:32:39 +0200

    INGM-410: commutation_test with different feedbacks configuration.

--- examples/commutation_test.py
+++ examples/commutation_test.py
@@ -2,9 +2,51 @@ from ingeniamotion import MotionController
 from ingeniamotion.enums import SensorType
 
 
+def use_only_incremental_encoders(mc: MotionController):
+    """All feedbacks are using an Incremental encoder.
+
+    Args:
+        mc: Controller to configure the type of encoder for each feedback
+
+    """
+    mc.configuration.set_auxiliar_feedback(SensorType.QEI)
+    mc.configuration.set_commutation_feedback(SensorType.QEI)
+    mc.configuration.set_position_feedback(SensorType.QEI)
+    mc.configuration.set_velocity_feedback(SensorType.QEI)
+    mc.configuration.set_reference_feedback(SensorType.QEI)
+
+
+def use_only_absolute_encoders(mc: MotionController):
+    """All feedbacks are using an Absolute encoder.
+
+    Args:
+        mc: Controller to configure the type of encoder for each feedback
+
+    """
+    mc.configuration.set_auxiliar_feedback(SensorType.ABS1)
+    mc.configuration.set_commutation_feedback(SensorType.ABS1)
+    mc.configuration.set_position_feedback(SensorType.ABS1)
+    mc.configuration.set_velocity_feedback(SensorType.ABS1)
+    mc.configuration.set_reference_feedback(SensorType.ABS1)
+
+
+def use_incremental_encoders_and_digital_halls(mc: MotionController):
+    """All feedbacks are using an incremental encoder and a digital halls.
+
+    Args:
+        mc: Controller to configure the type of encoder for each feedback
+
+    """
+    mc.configuration.set_auxiliar_feedback(SensorType.HALLS)
+    mc.configuration.set_commutation_feedback(SensorType.QEI)
+    mc.configuration.set_position_feedback(SensorType.HALLS)
+    mc.configuration.set_velocity_feedback(SensorType.QEI)
+    mc.configuration.set_reference_feedback(SensorType.QEI)
+
+
 def main() -> None:
     mc = MotionController()
-    
+
     interface_index = 3
     slave_id = 1
     dictionary_path = (
@@ -13,26 +55,23 @@ def main() -> None:
     mc.communication.connect_servo_ethercat_interface_index(
         interface_index, slave_id, dictionary_path
     )
-    
-    # Encoder configuration
-    # If you are using only one encoder (e.g.: Incremental Encoder), set the type of sensors
-    # using the sensor_type variable below.
-    # If you are using more than one encoder (e.g.: Inc. Enc. and Abs. Enc.), set the type of 
-    # sensors one by one.
-    sensor_type = SensorType.QEI 
-    mc.configuration.set_auxiliar_feedback(sensor_type)
-    mc.configuration.set_commutation_feedback(sensor_type)
-    mc.configuration.set_position_feedback(sensor_type)
-    mc.configuration.set_velocity_feedback(sensor_type)
-    mc.configuration.set_reference_feedback(sensor_type)
-    
+
+    # Feedbacks configuration:
+    # Indicate the type of encoder you are using for each feedback
+    # There are some examples of feedbacks configuration below.
+    # Uncomment the configuration you want to try:
+    # -------------------------------------------------------------
+    # use_only_incremental_encoders(mc)
+    # use_only_absolute_encoders(mc)
+    use_incremental_encoders_and_digital_halls(mc)
+    # -------------------------------------------------------------
+
     # Run Commutation test
     result = mc.tests.commutation()
-    print(result["result_message"])
+    print(f"Commutation Result: {result['result_message']}")
 
     mc.communication.disconnect()
 
 
 if __name__ == "__main__":
-    # Before executing this example, make sure your motor is calibrated.
     main()
