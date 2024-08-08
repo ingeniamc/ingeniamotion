98acaed342bd5b3dcb949037877a6d7463eedab8
Pol Feliu <>
2024-08-08 15:11:18 +0200

    asdf

--- examples/safety_torque_off.py
+++ examples/safety_torque_off.py
@@ -1,11 +1,10 @@
-import contextlib
+import time
 
 from ingenialink.enums.register import REG_ACCESS, REG_DTYPE
 from ingenialink.ethercat.register import EthercatRegister
 
 from ingeniamotion import MotionController
 from ingeniamotion.enums import OperationMode
-from ingeniamotion.exceptions import IMTimeoutError
 
 LAST_ERROR = EthercatRegister(
     identifier="LAST_ERROR",
@@ -30,34 +29,16 @@ def main(interface_ip, slave_id, dict_path):
     # Create and start the FSoE master handler
     mc.fsoe.create_fsoe_master_handler()
     mc.fsoe.configure_pdos(start_pdos=True)
-    # Wait for the master to reach the Data state
-    mc.fsoe.wait_for_state_data(timeout=10)
-    # Deactivate the SS1
-    mc.fsoe.ss1_deactivate()
-    # Deactivate the STO
-    mc.fsoe.sto_deactivate()
-    # Wait for the STO to be deactivated
-    while mc.fsoe.check_sto_active():
-        pass
-    # Enable the motor
-    mc.motion.motor_enable()
-    # Wait for the motor to reach a certain velocity (10 rev/s)
-    target_velocity = 10
-    mc.motion.set_velocity(target_velocity)
-    with contextlib.suppress(IMTimeoutError):
-        mc.motion.wait_for_velocity(velocity=target_velocity, timeout=5)
-    # Disable the motor
-    mc.motion.motor_disable()
-    # Activate the SS1
-    mc.fsoe.sto_activate()
-    # Activate the STO
-    mc.fsoe.sto_activate()
+    time.sleep(10)
+
     # Stop the FSoE master handler
     mc.fsoe.stop_master(stop_pdos=True)
 
     # Get last error
     drive = mc.servos["default"]
-    mc.communication.get_register(LAST_ERROR)
+    error = drive.read(LAST_ERROR)
+
+    print(error)
 
     # Restore the operation mode
     mc.motion.set_operation_mode(current_operation_mode)
