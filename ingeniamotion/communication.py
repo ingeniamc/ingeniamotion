eca36141118aa6baa79595bd7e7f43a64a222c9d
Javier Portillo Medina <javierportillo93@gmail.com>
2024-08-22 10:33:14 +0200

    INGM-498 Fix revision number check in __check_ensemble function

--- ingeniamotion/communication.py
+++ ingeniamotion/communication.py
@@ -1397,10 +1397,9 @@ class Communication(metaclass=MCMetaClass):
             if map_slave_id not in scanned_slaves:
                 raise IMException(f"Wrong ensemble. The slave {map_slave_id - 1} is not detected.")
             map_slave_info = scanned_slaves[map_slave_id]
-            if (
-                map_slave_info.product_code != mapping[map_slave_id_offset][1]
-                or map_slave_info.revision_number != mapping[map_slave_id_offset][2]
-            ):
+            if map_slave_info.product_code != mapping[map_slave_id_offset][1] or (
+                map_slave_info.revision_number & self.ENSEMBLE_SLAVE_REV_NUM_MASK
+            ) != (mapping[map_slave_id_offset][2] & self.ENSEMBLE_SLAVE_REV_NUM_MASK):
                 raise IMException(
                     f"Wrong ensemble. The slave {map_slave_id} "
                     f"has wrong product code or revision number."
