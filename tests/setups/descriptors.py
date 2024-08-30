ea183ff5d4097c53801f4ef6fa01f594d1a5fbec
Pol Feliu <>
2024-08-30 11:54:40 +0200

    remove configs json and parser

--- tests/setups/descriptors.py
+++ tests/setups/descriptors.py
@@ -1,21 +1,9 @@
 from dataclasses import dataclass
-from typing import Dict, List
 
 
 @dataclass
 class Setup:
-    @classmethod  # TODO Remove
-    def from_dict(cls, protocol_name: str, contents: Dict):
-        if protocol_name == "eoe":
-            return EoESetup(**contents, load_firmware_with_rack_service=False)
-        if protocol_name == "soem":
-            return SoemSetup(**contents, load_firmware_with_rack_service=False)
-        if protocol_name == "canopen":
-            return CanOpenSetup(**contents, load_firmware_with_rack_service=False)
-        if protocol_name == "virtual":
-            return VirtualDriveSetup(**contents)
-        else:
-            raise NotImplementedError
+    pass
 
 
 @dataclass
@@ -57,27 +45,6 @@ class CanOpenSetup(DriveHwSetup):
     channel: int
     node_id: int
     baudrate: int
-
-
-@dataclass
-class Protocol:  # TODO Remvoe
-    name: str
-    setups: List[Setup]
-
-    @classmethod
-    def from_dict(cls, name, contents: Dict):
-        return cls(name, [Setup.from_dict(name, setup) for setup in contents])
-
-
-@dataclass
-class Configs:  # TODO Remvoe
-    protocols: Dict[str, Protocol]
-
-    @classmethod
-    def from_dict(cls, contents: Dict):
-        return cls(
-            {name: Protocol.from_dict(name, protocol) for name, protocol in contents.items()}
-        )
 
 
 @dataclass
