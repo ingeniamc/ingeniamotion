0544f27ea101a6f755e530b9f3bf61469bfd80cf
Martin Acosta <martinacostallano@gmail.com>
2025-04-03 18:02:10 +0200

    INGM-596 Fix ruff error

--- tests/setups/virtual_drive.py
+++ tests/setups/virtual_drive.py
@@ -1,4 +1,5 @@
 from ingenialink.ethernet.network import VIRTUAL_DRIVE_DICTIONARY
+
 from tests.setups.descriptors import VirtualDriveSetup
 
 TESTS_SETUP = VirtualDriveSetup(ip="127.0.0.1", dictionary=VIRTUAL_DRIVE_DICTIONARY, port=1061)
