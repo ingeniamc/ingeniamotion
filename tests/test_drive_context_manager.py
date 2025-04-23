b79d945c1ef581bfdc4020bc14d3e3814a57340e
julieta.prieto <julieta.prieto@novanta.com>
2025-04-23 08:32:05 +0200

    removed wrong files

--- tests/test_drive_context_manager.py
+++
@@ -1,9 +0,0 @@
-from ingeniamotion.drive_context_manager import DriveContextManager
-
-
-def test_drive_context_manager(motion_controller):
-    mc, alias, environment = motion_controller
-    context = DriveContextManager(motion_controller=mc)
-
-    with context:
-        aa = 0
