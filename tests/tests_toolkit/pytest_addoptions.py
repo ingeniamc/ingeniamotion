709f43171f53db81ca69285b08c2c7e0cceb7d99
julieta.prieto <julieta.prieto@novanta.com>
2025-04-24 09:18:39 +0200

    move common addoptions to another file

---
+++ tests/tests_toolkit/pytest_addoptions.py
@@ -0,0 +1,10 @@
+def pytest_addoption(parser):
+    parser.addoption(
+        "--setup",
+        action="store",
+        default="tests.setups.tests_setup.TESTS_SETUP",
+        help="Module and location from which to import the setup."
+        "It will default to a file that you can create on"
+        "tests_setup.py inside of the folder setups with a variable called TESTS_SETUP"
+        "This variable must define, or must be assigned to a Setup instance",
+    )
