5e012cac3db861ce55d0b3244fa2524027cf385e
Martin Acosta <martinacostallano@gmail.com>
2025-06-18 16:14:20 +0200

    INGM-645 Update version

--- ingeniamotion/__init__.py
+++ ingeniamotion/__init__.py
@@ -1,7 +1,7 @@
 from . import enums
 from .motion_controller import MotionController
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 """ str: Library version. """
 
 __all__ = ["MotionController", "enums"]
