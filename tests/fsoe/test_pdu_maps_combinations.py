a25a58b3bc510c44cd93c8daa259a578fe40f329
julieta.prieto <julieta.prieto@novanta.com>
2025-08-04 09:28:17 +0200

    INGM-656 add padding logic

--- tests/fsoe/test_pdu_maps_combinations.py
+++ tests/fsoe/test_pdu_maps_combinations.py
@@ -9,6 +9,13 @@ from ingeniamotion.motion_controller import MotionController
 
 if FSOE_MASTER_INSTALLED:
     import ingeniamotion.fsoe_master.safety_functions as safety_functions
+    from ingeniamotion.fsoe_master.fsoe import (
+        FSoEDictionaryItem,
+        FSoEDictionaryItemInput,
+        FSoEDictionaryItemOutput,
+        FSoEDictionaryMap,
+        align_bits,
+    )
     from ingeniamotion.fsoe_master.handler import FSoEMasterHandler
     from ingeniamotion.fsoe_master.safety_functions import SafetyFunction
 
@@ -16,7 +23,143 @@ if FSOE_MASTER_INSTALLED:
 logger = get_logger(__name__)
 
 
-def generate_random_mapping(handler: FSoEMasterHandler, max_items) -> None:
+def _insert_item_according_to_fsoe_rules(
+    dictionary_map: FSoEDictionaryMap, item: FSoEDictionaryItem
+) -> bool:
+    """Insert the item into the appropriate map according to FSoE rules.
+
+    Args:
+        dictionary_map: The FSoE dictionary map.
+        item: The item to insert.
+
+    Returns:
+        True if item was added, False otherwise.
+    """
+    if not len(dictionary_map._items):
+        dictionary_map.add(item)
+        return True
+
+    # According to FSoE Frame construction rules, maximum 8 data blocks are allowed in a frame
+    last_item = dictionary_map._items[-1]
+    current_position_bits = last_item.position_bits + last_item.bits
+    # Check if adding this item would exceed the frame size
+    if (current_position_bits + item.data_type.bits) > 8 * 16:
+        return False
+
+    # If the item is smaller than 16 bits, it cannot be split in different data blocks
+    if item.data_type.bits < 16:
+        # Check if adding this item would cause it to span across data blocks (16-bit boundaries)
+        data_block_boundary = ((current_position_bits // 16) + 1) * 16
+        if (current_position_bits + item.data_type.bits) > data_block_boundary:
+            # Item would span across data blocks, add padding to align to next boundary
+            n_bits_padding = data_block_boundary - current_position_bits
+            if (current_position_bits + n_bits_padding + item.data_type.bits) <= 8 * 16:
+                dictionary_map.add_padding(n_bits_padding)
+            else:
+                return False
+        dictionary_map.add(item)
+        return True
+
+    next_alignment = align_bits(current_position_bits, 16)
+    n_bits_padding = next_alignment - current_position_bits
+    # Add padding if it fits in the frame, otherwise just add the item
+    if (
+        n_bits_padding > 0
+        and (current_position_bits + item.data_type.bits + n_bits_padding) <= 8 * 16
+    ):
+        dictionary_map.add_padding(n_bits_padding)
+    dictionary_map.add(item)
+    return True
+
+
+def _insert_padding_according_to_fsoe_rules(
+    dictionary_map: FSoEDictionaryMap, padding_size: int
+) -> None:
+    """Insert padding into the dictionary map according to FSoE rules.
+
+    Args:
+        dictionary_map: The FSoE dictionary map.
+        padding_size: The size of the padding to insert in bits.
+    """
+    if not len(dictionary_map._items):
+        dictionary_map.add_padding(padding_size)
+        return
+
+    # According to FSoE Frame construction rules, maximum 8 data blocks are allowed in a frame
+    last_item = dictionary_map._items[-1]
+    current_position_bits = last_item.position_bits + last_item.bits
+    # Check if adding this item would exceed the frame size
+    if (current_position_bits + padding_size) > 8 * 16:
+        return
+
+    # If the item is smaller than 16 bits, it cannot be split in different data blocks
+    if padding_size < 16:
+        # Check if adding this item would cause it to span across data blocks (16-bit boundaries)
+        data_block_boundary = ((current_position_bits // 16) + 1) * 16
+        if (current_position_bits + padding_size) > data_block_boundary:
+            n_bits_padding = data_block_boundary - current_position_bits
+            # If item would span across data blocks,
+            # just add padding to align to next boundary
+            if n_bits_padding > 0:
+                dictionary_map.add_padding(n_bits_padding)
+            else:
+                return
+        else:
+            dictionary_map.add_padding(padding_size)
+        return
+
+    next_alignment = align_bits(current_position_bits, 16)
+    n_bits_padding = next_alignment - current_position_bits
+    # If padding is not aligned, just add the remaining bits for the alignment
+    if n_bits_padding > 0:
+        dictionary_map.add_padding(n_bits_padding)
+    else:
+        dictionary_map.add_padding(padding_size)
+
+
+def _add_random_item_to_map(
+    handler: FSoEMasterHandler, item: FSoEDictionaryItem, random_paddings: bool
+) -> None:
+    """Add a random item to the map with optional padding.
+
+    Args:
+        handler: The FSoE master handler.
+        item: The item that will be added to the map.
+        random_paddings: True to add random paddings to the mapping.
+    """
+    add_padding = random_paddings and random.choice([True, False])
+
+    if add_padding:
+        # Random padding size between 1-16 bits
+        padding_size = random.randint(1, 16)
+
+        # Add the item + padding
+        if isinstance(item, FSoEDictionaryItemInput):
+            added = _insert_item_according_to_fsoe_rules(handler.maps.inputs, item)
+            if added:
+                _insert_padding_according_to_fsoe_rules(handler.maps.inputs, padding_size)
+        elif isinstance(item, FSoEDictionaryItemOutput):
+            added = _insert_item_according_to_fsoe_rules(handler.maps.outputs, item)
+            if added:
+                _insert_padding_according_to_fsoe_rules(handler.maps.outputs, padding_size)
+        else:
+            input_added = _insert_item_according_to_fsoe_rules(handler.maps.inputs, item)
+            output_added = _insert_item_according_to_fsoe_rules(handler.maps.outputs, item)
+
+            # FSoEDictionaryItemInputOutput - add padding to random map, not to both
+            if random.choice([True, False]):
+                if input_added:
+                    _insert_padding_according_to_fsoe_rules(handler.maps.inputs, padding_size)
+            elif output_added:
+                _insert_padding_according_to_fsoe_rules(handler.maps.outputs, padding_size)
+    else:
+        # Insert the item in the best position without padding
+        handler.maps.insert_in_best_position(item)
+
+
+def generate_random_mapping(
+    handler: FSoEMasterHandler, max_items: int, random_paddings: bool
+) -> None:
     """Generate a random mapping of safety functions, adding 1 of each data type randomly.
 
     When a data type is finished, continue with the remaining ones.
@@ -24,6 +167,8 @@ def generate_random_mapping(handler: FSoEMasterHandler, max_items) -> None:
     Args:
         handler: The FSoE master handler.
         max_items: Maximum number of items to add to the mapping.
+        random_paddings: True to add random paddings to the mapping.
+            Insert in best position will be used otherwise.
     """
     items_added = 0
 
@@ -38,7 +183,7 @@ def generate_random_mapping(handler: FSoEMasterHandler, max_items) -> None:
     if safety_functions.STOFunction.COMMAND_UID in handler.dictionary.name_map:
         # Add the STO command to the outputs map first
         sto_command = handler.dictionary.name_map[safety_functions.STOFunction.COMMAND_UID]
-        handler.maps.insert_in_best_position(sto_command)
+        _add_random_item_to_map(handler=handler, item=sto_command, random_paddings=random_paddings)
         safety_io[sto_command.data_type].remove(sto_command)
         items_added += 1
 
@@ -57,7 +202,9 @@ def generate_random_mapping(handler: FSoEMasterHandler, max_items) -> None:
         selected_item = random.choice(safety_io[selected_type])
 
         # Add the item to the mapping
-        handler.maps.insert_in_best_position(selected_item)
+        _add_random_item_to_map(
+            handler=handler, item=selected_item, random_paddings=random_paddings
+        )
         safety_io[selected_type].remove(selected_item)  # Item already added
         items_added += 1
 
@@ -73,7 +220,7 @@ def test_map_safety_input_output_random(
     handler.maps.outputs.clear()
 
     # Generate a random mapping
-    generate_random_mapping(handler=handler, max_items=1)
+    generate_random_mapping(handler=handler, max_items=5, random_paddings=True)
     handler.maps.validate()
 
     mc.fsoe.configure_pdos()
