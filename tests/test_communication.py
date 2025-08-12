ab840998511e8a9985ca99434afd9891db6bfcf9
julieta.prieto <julieta.prieto@novanta.com>
2025-08-12 07:26:02 +0200

    INGM-656 fix pytest marker

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -123,7 +123,7 @@ def test_connect_servo_ethernet(setup_descriptor: EthernetSetup):
     assert "ethernet_test" in mc.net and mc.net["ethernet_test"] is not None
 
 
-@pytest.mark.ethercat
+@pytest.mark.soem
 @pytest.mark.skip_testing_framework
 def test_mc_disconnects_with_disconnection_callback(setup_descriptor: DriveEcatSetup, alias: str):
     mc = MotionController()
