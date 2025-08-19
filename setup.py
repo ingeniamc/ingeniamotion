9c41a3d46d5567ae734ef81d4f66fa27a014238f
Pol Feliu <pol.feliu@novanta.com>
2025-08-19 15:43:45 +0200

    INGK-1159 update fsoe master

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         "ifaddr==0.1.7",
     ],
     extras_require={
-        "FSoE": ["fsoe_master==0.1.4+pr75b2"],
+        "FSoE": ["fsoe_master==0.1.4+g9115368a2"],
     },
     python_requires=">=3.9",
 )
