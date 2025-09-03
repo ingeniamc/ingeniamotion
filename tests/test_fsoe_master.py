943ce967cb48866a83bcd48c6265ca3e4ef556c7
Javier Portillo Medina <javierportillo93@gmail.com>
2025-09-03 18:30:23 +0200

    INGM-681 Improve test

--- tests/test_fsoe_master.py
+++ tests/test_fsoe_master.py
@@ -611,28 +611,40 @@ def test_modify_safe_parameters(fsoe_error_monitor: Callable[[FSoEError], None])
 
 
 @pytest.mark.fsoe_phase2
-@pytest.mark.parametrize(
-    "register_uid, is_wrote",
-    [
-        ("FSOE_SS1_TIME_TO_STO_1", True),
-        ("FSOE_SS1_DEC_LIMIT_1", True),
-        ("ETG_COMMS_RPDO_MAP256_1", False),
-        ("ETG_COMMS_RPDO_MAP256_2", False),
-        ("ETG_COMMS_TPDO_MAP256_3", False),
-        ("ETG_COMMS_TPDO_MAP256_4", False),
-        ("FSOE_SLP_UPPER_LIMIT_8", True),
-        ("FSOE_SLI_LOWER_LIMIT_1", True),
-    ],
-)
-def test_write_safe_parameters(mc_with_fsoe, register_uid, is_wrote):
+def test_write_safe_parameters(mc_with_fsoe):
     mc, handler = mc_with_fsoe
-    old_val = mc.communication.get_register(register_uid)
-    new_val = old_val + 1
-    handler.safety_parameters[register_uid].set_without_updating(new_val)
-    expected_value = new_val if is_wrote else old_val
+    expected_value = {}
+    for key, param in handler.safety_parameters.items():
+        old_val = mc.communication.get_register(key)
+
+        # Remove if when SACOAPP-299 is completed
+        if key == "FSOE_SSR_ERROR_REACTION_8":
+            param.register._enums = {
+                "STO": 0x66400001,
+                "SS1": 0x66500101,
+                "No reaction": 0x0
+            }
+        # Remove if when SACOAPP-300 is completed
+        if key == "FSOE_SS2_ERROR_REACTION_1":
+            param.register._enums = {
+                "STO": 0x66400001,
+                "No reaction": 0x0
+            }
+        if param.register.enums:
+            enum_values = list(param.register.enums.values())
+            enum_values.remove(old_val)
+            new_val = enum_values[0]
+        else:
+            new_val = old_val - 1 if old_val == param.register.range[1] else old_val + 1
+        param.set_without_updating(new_val)
+        if param.register.idx in [0x1700, 0x1B00]:
+            expected_value[key] = old_val
+        else:
+            expected_value[key] = new_val
     handler.write_safe_parameters()
-    test_val = mc.communication.get_register(register_uid)
-    assert test_val == expected_value, f"Parameter {register_uid} not written correctly"
+    for key, param in handler.safety_parameters.items():
+        test_val = mc.communication.get_register(key)
+        assert test_val == expected_value[key], f"Parameter {key} not written correctly"
 
 
 @pytest.mark.fsoe
