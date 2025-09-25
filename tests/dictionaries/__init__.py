22b18b3f3482c0d3aa25331a3bdfc72bc180fe3a
julieta.prieto <julieta.prieto@novanta.com>
2025-09-25 16:31:37 +0200

    INGM-708 update ref dict

--- tests/dictionaries/__init__.py
+++ tests/dictionaries/__init__.py
@@ -7,6 +7,6 @@ SAMPLE_SAFE_PH1_XDFV3_DICTIONARY = (
     __dictionaries_root_path / "den-s-net-e_devf13ab4_v3.xdf"
 ).as_posix()
 SAMPLE_SAFE_PH2_XDFV3_DICTIONARY = (
-    __dictionaries_root_path / "evs-s-net-e_2.9.0.006_v3.xdf"
+    __dictionaries_root_path / "evs-s-net-e_2.9.0.009_v3.xdf"
 ).as_posix()
 SAMPLE_SAFE_PH2_MODULE_IDENT_NO_SRA_MODULE_IDENT = 0x3B00003
