71e24ae2b960c0158f2df889903080e08baada8b
MartinALL91 <martinacostallano@gmail.com>
2026-02-27 15:09:53 +0100

    Fix dictionary

--- tests/setups/virtual_drive.py
+++ tests/setups/virtual_drive.py
@@ -1,5 +1,7 @@
 from summit_testing_framework.setups.specifiers import VirtualDriveSpecifier
 
+from tests.dictionaries import VIRTUAL_DRIVE_XDF_PATH
+
 VIRTUAL_DRIVE_ETHERNET_SETUP = VirtualDriveSpecifier.from_ethernet(
-    extra_data={"execution_policy": "always"}
+    dictionary=VIRTUAL_DRIVE_XDF_PATH, extra_data={"execution_policy": "always"}
 )
