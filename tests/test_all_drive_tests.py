ce35e2939088cb9c41d09bbef617c848efa5b3bc
Pol Feliu Cuberes <>
2026-06-22 20:04:23 +0200

    chore(deps): bump summit-testing-framework 0.3.0.post243+develop.82ae762 -> 0.3.0.post249+pr.44.b1

--- tests/test_all_drive_tests.py
+++ tests/test_all_drive_tests.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 from ingenialink import exceptions
-from ingenialink.drive_context_manager import DriveRegistersValues
+from ingenialink.drive_context_manager import DriveRegistersValue
 from ingenialink.servo import Servo
 
 from ingeniamotion.enums import SensorType, SeverityLevel
@@ -52,9 +52,9 @@ def feedback_test_setup(
     mc.tests.commutation(servo=environment.aliases)
 
 
-def assert_test_returns_to_baseline(servo: Servo, baseline: DriveRegistersValues):
+def assert_test_returns_to_baseline(servo: Servo, baseline: DriveRegistersValue):
     """Assert that the test returns to the baseline configuration after running."""
-    current_state = DriveRegistersValues.from_hardware(servo)
+    current_state = DriveRegistersValue.from_hardware(servo)
 
     differences = baseline.diff(current_state)
 
@@ -73,7 +73,7 @@ def assert_test_returns_to_baseline(servo: Servo, baseline: DriveRegistersValues
 @pytest.mark.usefixtures("feedback_test_setup")
 # https://novantamotion.atlassian.net/browse/INGM-782
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
-def test_digital_halls_test(mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues):
+def test_digital_halls_test(mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue):
     commutation_fdbk = mc.configuration.get_commutation_feedback(servo=alias)
     if SensorType.HALLS in feedback_list:
         results = mc.tests.digital_halls_test(servo=alias)
@@ -93,7 +93,7 @@ def test_digital_halls_test(mc, alias, feedback_list, servo: Servo, baseline: Dr
 # https://novantamotion.atlassian.net/browse/INGM-783
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
 def test_incremental_encoder_1_test(
-    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues
+    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue
 ):
     commutation_fdbk = mc.configuration.get_commutation_feedback(servo=alias)
     if SensorType.QEI in feedback_list:
@@ -114,7 +114,7 @@ def test_incremental_encoder_1_test(
 # https://novantamotion.atlassian.net/browse/INGM-784
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
 def test_incremental_encoder_2_test(
-    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues
+    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue
 ):
     if not mc.info.register_exists("FBK_DIGENC2_RESOLUTION", servo=alias):
         pytest.skip("Incremental encoder 2 is not available")
@@ -137,7 +137,7 @@ def test_incremental_encoder_2_test(
 # https://novantamotion.atlassian.net/browse/INGM-785
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
 def test_absolute_encoder_1_test(
-    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues
+    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue
 ):
     commutation_fdbk = mc.configuration.get_commutation_feedback(servo=alias)
     if SensorType.ABS1 in feedback_list:
@@ -158,7 +158,7 @@ def test_absolute_encoder_1_test(
 # https://novantamotion.atlassian.net/browse/INGM-786
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
 def test_absolute_encoder_2_test(
-    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues
+    mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue
 ):
     commutation_fdbk = mc.configuration.get_commutation_feedback(servo=alias)
     if SensorType.BISSC2 in feedback_list:
@@ -178,7 +178,7 @@ def test_absolute_encoder_2_test(
 @pytest.mark.usefixtures("feedback_test_setup")
 # https://novantamotion.atlassian.net/browse/INGM-787
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
-def test_secondary_ssi_test(mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValues):
+def test_secondary_ssi_test(mc, alias, feedback_list, servo: Servo, baseline: DriveRegistersValue):
     commutation_fdbk = mc.configuration.get_commutation_feedback(servo=alias)
     if SensorType.QEI in feedback_list:
         pytest.skip("Can not run the test. Incremental encoder 1 and SSI 2 share pins.")
@@ -199,7 +199,7 @@ def test_secondary_ssi_test(mc, alias, feedback_list, servo: Servo, baseline: Dr
 # https://novantamotion.atlassian.net/browse/INGM-774
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
 def test_commutation(
-    alias: str, mc: "MotionController", servo: Servo, baseline: DriveRegistersValues
+    alias: str, mc: "MotionController", servo: Servo, baseline: DriveRegistersValue
 ) -> None:
     results = mc.tests.commutation(servo=alias)
     assert results["result_severity"] == SeverityLevel.SUCCESS
@@ -210,7 +210,7 @@ def test_commutation(
 @pytest.mark.ethernet
 @pytest.mark.soem
 @pytest.mark.canopen
-def test_commutation_error(mc, alias, force_fault, servo: Servo, baseline: DriveRegistersValues):
+def test_commutation_error(mc, alias, force_fault, servo: Servo, baseline: DriveRegistersValue):
     with pytest.raises(force_fault):
         mc.tests.commutation(servo=alias)
 
@@ -221,7 +221,7 @@ def test_commutation_error(mc, alias, force_fault, servo: Servo, baseline: Drive
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.skip("Skip until is fixed INGM-352")
-def test_phasing_check(mc, alias, servo: Servo, baseline: DriveRegistersValues):
+def test_phasing_check(mc, alias, servo: Servo, baseline: DriveRegistersValue):
     mc.tests.commutation(servo=alias)
     results = mc.tests.phasing_check(servo=alias)
     assert results["result_severity"] == SeverityLevel.SUCCESS
@@ -231,7 +231,7 @@ def test_phasing_check(mc, alias, servo: Servo, baseline: DriveRegistersValues):
 @pytest.mark.ethernet
 @pytest.mark.soem
 @pytest.mark.canopen
-def test_phasing_check_error(mc, alias, force_fault, servo: Servo, baseline: DriveRegistersValues):
+def test_phasing_check_error(mc, alias, force_fault, servo: Servo, baseline: DriveRegistersValue):
     with pytest.raises(force_fault):
         mc.tests.phasing_check(servo=alias)
 
@@ -241,7 +241,7 @@ def test_phasing_check_error(mc, alias, force_fault, servo: Servo, baseline: Dri
 @pytest.mark.ethernet
 @pytest.mark.soem
 @pytest.mark.canopen
-def test_sto_test(mc, alias, servo: Servo, baseline: DriveRegistersValues):
+def test_sto_test(mc, alias, servo: Servo, baseline: DriveRegistersValue):
     results = mc.tests.sto_test(servo=alias)
     assert results["result_severity"] == SeverityLevel.SUCCESS
 
@@ -260,7 +260,7 @@ def test_sto_test(mc, alias, servo: Servo, baseline: DriveRegistersValues):
     ],
 )
 def test_sto_test_error(
-    mocker, mc, alias, sto_value, message, servo: Servo, baseline: DriveRegistersValues
+    mocker, mc, alias, sto_value, message, servo: Servo, baseline: DriveRegistersValue
 ):
     mocker.patch("ingeniamotion.configuration.Configuration.get_sto_status", return_value=sto_value)
     results = mc.tests.sto_test(servo=alias)
@@ -273,7 +273,7 @@ def test_sto_test_error(
 @pytest.mark.virtual
 @pytest.mark.parametrize("sto_value", [0x4, 0x1F, 0xE, 0x73, 0x5, 0x17])
 def test_sto_test_logs(
-    caplog, mocker, mc, alias, sto_value, servo: Servo, baseline: DriveRegistersValues
+    caplog, mocker, mc, alias, sto_value, servo: Servo, baseline: DriveRegistersValue
 ):
     caplog.set_level(logging.INFO)
     mocker.patch("ingeniamotion.configuration.Configuration.get_sto_status", return_value=sto_value)
@@ -299,7 +299,7 @@ def test_sto_test_logs(
 @pytest.mark.ethernet
 @pytest.mark.soem
 @pytest.mark.canopen
-def test_brake_test(mc, alias, servo: Servo, baseline: DriveRegistersValues):
+def test_brake_test(mc, alias, servo: Servo, baseline: DriveRegistersValue):
     # Set frame type to BiSS-C BP3 to ensure that the test changes it to avoid an error.
     mc.communication.set_register("FBK_BISS1_SSI1_FRAME_TYPE", 3, servo=alias)
     pair_poles = mc.configuration.get_motor_pair_poles(servo=alias)
@@ -336,7 +336,7 @@ def run_test_and_stop(test):
 )
 # https://novantamotion.atlassian.net/browse/INGM-790
 @pytest.mark.not_valid_for_product(part_number="CAP-XCR-E")
-def test_feedback_stop(mc, alias, feedback_class, servo: Servo, baseline: DriveRegistersValues):
+def test_feedback_stop(mc, alias, feedback_class, servo: Servo, baseline: DriveRegistersValue):
     test = feedback_class(mc, alias, 1)
     run_test_and_stop(test)
 
@@ -344,7 +344,7 @@ def test_feedback_stop(mc, alias, feedback_class, servo: Servo, baseline: DriveR
 
 
 @pytest.mark.virtual
-def test_commutation_stop(mc, alias, servo: Servo, baseline: DriveRegistersValues):
+def test_commutation_stop(mc, alias, servo: Servo, baseline: DriveRegistersValue):
     test = Phasing(mc, alias, 1)
     run_test_and_stop(test)
 
@@ -352,7 +352,7 @@ def test_commutation_stop(mc, alias, servo: Servo, baseline: DriveRegistersValue
 
 
 @pytest.mark.virtual
-def test_phasing_check_stop(mc, alias, servo: Servo, baseline: DriveRegistersValues):
+def test_phasing_check_stop(mc, alias, servo: Servo, baseline: DriveRegistersValue):
     test = PhasingCheck(mc, alias, 1)
     run_test_and_stop(test)
 
@@ -373,7 +373,7 @@ def test_phasing_check_stop(mc, alias, servo: Servo, baseline: DriveRegistersVal
     ],
 )
 def test_current_ramp_up(
-    mc, alias, test_currents, test_sensor, servo: Servo, baseline: DriveRegistersValues
+    mc, alias, test_currents, test_sensor, servo: Servo, baseline: DriveRegistersValue
 ):
     axis = 1
     test_feedback_options = {
--- tests/test_feedback_test.py
+++ tests/test_feedback_test.py
@@ -2,68 +2,20 @@ import pytest
 
 from ingeniamotion.enums import SensorType
 from ingeniamotion.wizard_tests.base_test import TestConfigurationError
-from ingeniamotion.wizard_tests.feedbacks_tests.absolute_encoder1_test import (
-    AbsoluteEncoder1Test,
-)
-from ingeniamotion.wizard_tests.feedbacks_tests.absolute_encoder2_test import (
-    AbsoluteEncoder2Test,
-)
 from ingeniamotion.wizard_tests.feedbacks_tests.dc_feedback_polarity_test import (
     DCFeedbacksPolarityTest,
 )
 from ingeniamotion.wizard_tests.feedbacks_tests.dc_feedback_resolution_test import (
     DCFeedbacksResolutionTest,
 )
-from ingeniamotion.wizard_tests.feedbacks_tests.digital_hall_test import DigitalHallTest
 from ingeniamotion.wizard_tests.feedbacks_tests.digital_incremental1_test import (
     DigitalIncremental1Test,
-)
-from ingeniamotion.wizard_tests.feedbacks_tests.digital_incremental2_test import (
-    DigitalIncremental2Test,
-)
-from ingeniamotion.wizard_tests.feedbacks_tests.secondary_ssi_test import (
-    SecondarySSITest,
 )
 
 # Record stop opportunities for every wizard-test integration case in this module.
 pytestmark = pytest.mark.usefixtures("stoppable_trace_recorder")
 
 INCREMENTAL_ENCODER_1_RESOLUTION_REGISTER = "FBK_DIGENC1_RESOLUTION"
-
-
-@pytest.mark.virtual
-@pytest.mark.parametrize(
-    "feedback_test_type",
-    [
-        DigitalIncremental1Test,
-        DigitalIncremental2Test,
-        AbsoluteEncoder1Test,
-        AbsoluteEncoder2Test,
-        SecondarySSITest,
-        DigitalHallTest,
-    ],
-)
-def test_save_backup_registers(mc, alias, feedback_test_type):
-    axis = 1
-    feedback_test = feedback_test_type(mc, alias, axis)
-    mandatory_backup_registers = feedback_test.backup_registers_names
-
-    expected_total_mandatory = len(mandatory_backup_registers)
-    expected_total_optional = 3
-    expected_total_backup_registers = expected_total_mandatory + expected_total_optional
-
-    feedback_test.save_backup_registers()
-    saved_backup_registers = feedback_test.backup_registers[axis]
-    total_backup_registers = len(saved_backup_registers)
-
-    assert total_backup_registers == expected_total_backup_registers
-    for expected_register in feedback_test.backup_registers_names:
-        assert expected_register in saved_backup_registers
-    assert "COMMU_ANGLE_INTEGRITY1_OPTION" not in saved_backup_registers
-    assert "COMMU_ANGLE_INTEGRITY2_OPTION" not in saved_backup_registers
-    assert "PROF_POS_OPTION_CODE" in saved_backup_registers
-    assert "CL_POS_REF_MAX_RANGE" in saved_backup_registers
-    assert "CL_POS_REF_MIN_RANGE" in saved_backup_registers
 
 
 @pytest.mark.virtual
