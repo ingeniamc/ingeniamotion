5bfe080a7ab6dd97bb19fbdae5e73dbfe0c4e24c
julieta.prieto <julieta.prieto@novanta.com>
2026-07-07 12:14:11 +0200

    CIT-713 submodule update

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -1,4 +1,3 @@
-import summit_drives_ci_configs.config_files as config_files
 from ingenialink.dictionary import Interface
 from summit_testing_framework.jenkins.pytest_config import PyTestConfig
 from summit_testing_framework.setups.specifier_container import SpecifierContainer
@@ -9,6 +8,8 @@ from summit_testing_framework.setups.specifiers import (
     RackServiceConfigSpecifier,
     VersionConfig,
 )
+
+import summit_drives_ci_configs.config_files as config_files
 
 __EXECUTION_POLICY_KEY: str = "execution_policy"
 __TEST_CONFIGS_KEY: str = "test_configs"
