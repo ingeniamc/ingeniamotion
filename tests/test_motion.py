9ef53a54eea2c91bdb1cbbd36ce8e21e202628bd
julieta.prieto <julieta.prieto@novanta.com>
2026-07-23 09:01:45 +0200

    INGM-778 repeat marker

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -276,6 +276,7 @@ def test_set_position(mc, alias, position_value):
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.parametrize("position_value", [1000, 0, -1000, 4000])
+@pytest.mark.repeat(300)
 def test_move_position(mc, alias, position_value):
     pos_res = mc.configuration.get_position_feedback_resolution(servo=alias)
     mc.motion.set_operation_mode(OperationMode.PROFILE_POSITION, servo=alias)
