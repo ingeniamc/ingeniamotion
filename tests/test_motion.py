b197166a8b43ef44d0f9871fff63cbcdeac4e321
julieta.prieto <julieta.prieto@novanta.com>
2026-08-03 10:44:53 +0200

    INGM-780 repeat

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -382,6 +382,7 @@ def test_ramp_generator(mocker, init_v, final_v, total_t, t, result):
 @pytest.mark.biss_c_flaky(
     "Sporadically fails on ABS BiSS-C config, will be skipped for certain firmware versions"
 )
+@pytest.mark.repeat(100)
 def test_get_actual_position(mc, alias, position_value):
     mc.motion.set_operation_mode(OperationMode.PROFILE_POSITION, servo=alias)
     mc.motion.motor_enable(servo=alias)
