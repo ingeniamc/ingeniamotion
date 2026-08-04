103dfc73dc0dd7b4fa415f94a1cdb1bfe7458558
julieta.prieto <julieta.prieto@novanta.com>
2026-08-04 11:50:45 +0200

    INGM-780 reduce repeat + timeout

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -382,14 +382,14 @@ def test_ramp_generator(mocker, init_v, final_v, total_t, t, result):
 @pytest.mark.biss_c_flaky(
     "Sporadically fails on ABS BiSS-C config, will be skipped for certain firmware versions"
 )
-@pytest.mark.repeat(100)
+@pytest.mark.repeat(20)
 def test_get_actual_position(mc, alias, position_value):
     mc.motion.set_operation_mode(OperationMode.PROFILE_POSITION, servo=alias)
     mc.motion.motor_enable(servo=alias)
     mc.motion.move_to_position(position_value, servo=alias, blocking=True, timeout=10)
 
     # Wait for the position to stabilize
-    stability_timeout = 10
+    stability_timeout = 3
     stability_interval = 0.1
     stable_samples_required = 5
     stable_positions = []
