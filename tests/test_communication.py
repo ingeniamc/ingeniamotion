c118c0ef24289034b923d1ce2bacef418992e30b
julieta.prieto <julieta.prieto@novanta.com>
2026-08-27 09:36:32 +0200

    INGM-789 remove xfail

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -809,9 +809,8 @@ def test_subscribe_register_updates(mc: "MotionController", alias: str) -> None:
 
 @pytest.mark.canopen
 @pytest.mark.soem
-@pytest.mark.not_valid_for_product(
-    part_number="EVE-XCR-E", xfail="https://novantamotion.atlassian.net/browse/DRIVSUS-392"
-)
+# https://novantamotion.atlassian.net/browse/DRIVSUS-392
+@pytest.mark.not_valid_for_product(part_number="EVE-XCR-E")
 def test_emcy_callback(mc: "MotionController", alias: str) -> None:
     emcy_test = EmcyTest()
     prev_val = mc.communication.get_register("DRV_PROT_USER_OVER_VOLT", axis=1, servo=alias)
