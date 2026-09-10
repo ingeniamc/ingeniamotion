be7a1d331f2cce850abd0ad9c2080e44e94dc9d7
julieta.prieto <julieta.prieto@novanta.com>
2026-09-10 10:06:57 +0200

    fix invalid version for EVE in test_homing_on_index_pulse

--- tests/test_homing.py
+++ tests/test_homing.py
@@ -309,8 +309,7 @@ def test_current_position_homing_restores_operation_mode_on_failure(mocker) -> N
 @pytest.mark.parametrize("direction", [1, 0])
 @pytest.mark.not_valid_version_for_product(
     part_number="EVE-*",
-    min="2.7.0",
-    max="2.8.1",
+    min="2.9.0",
     skip_reason="https://novantamotion.atlassian.net/browse/COMOCOAPP-493 (fixed in 2.9.0)",
 )
 def test_homing_on_index_pulse(servo, mc, alias, feedback_list, direction):
