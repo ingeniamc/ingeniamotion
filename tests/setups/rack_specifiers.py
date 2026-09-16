8313229092283a05ced7ebdced92f6abf6c57317
Pol Feliu Cuberes <>
2026-09-16 11:22:54 +0200

    MOT3-5670 Fix test import ordering

--- tests/setups/rack_specifiers.py
+++ tests/setups/rack_specifiers.py
@@ -1,6 +1,3 @@
-from tests.conftest import BISS_C_CONFIG_MARKER, RANDOM_COMBINATIONS_SLICE_KEY
-
-import summit_drives_ci_configs.config_files as config_files
 from ingenialink.dictionary import Interface
 from summit_testing_framework.configuration.encoder_configurator import (
     EncoderConfiguration,
@@ -21,6 +18,9 @@ from summit_testing_framework.setups.specifiers import (
     RackServiceConfigSpecifier,
     VersionConfig,
 )
+
+import summit_drives_ci_configs.config_files as config_files
+from tests.conftest import BISS_C_CONFIG_MARKER, RANDOM_COMBINATIONS_SLICE_KEY
 
 __EXECUTION_POLICY_KEY: str = "execution_policy"
 __TEST_CONFIGS_KEY: str = "test_configs"
