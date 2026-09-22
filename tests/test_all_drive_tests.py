214fc0eefe12a173a736d96d37b2edbe76aa687f
julieta.prieto <julieta.prieto@novanta.com>
2026-09-22 12:43:35 +0200

    INGM-783 add pytest repeat marker to test_incremental_encoder_1_test

--- tests/test_all_drive_tests.py
+++ tests/test_all_drive_tests.py
@@ -163,10 +163,7 @@ def test_digital_halls_test(
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.usefixtures("feedback_test_setup")
-@pytest.mark.not_valid_for_specifier(
-    specifier="tests.setups.rack_specifiers.ECAT_SETUP@CAP-XCR-E",
-    skip_reason="https://novantamotion.atlassian.net/browse/INGM-783",
-)
+@pytest.mark.repeat(100)
 def test_incremental_encoder_1_test(
     mc,
     alias,
