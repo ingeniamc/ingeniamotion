53aeb13fa6103db6f6780adf537d735db16f4ace
julieta.prieto <julieta.prieto@novanta.com>
2026-09-23 14:18:47 +0200

    INGM-786 add repeat marker

--- tests/test_all_drive_tests.py
+++ tests/test_all_drive_tests.py
@@ -252,10 +252,7 @@ def test_absolute_encoder_1_test(
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.usefixtures("feedback_test_setup")
-@pytest.mark.not_valid_for_specifier(
-    specifier="tests.setups.rack_specifiers.ECAT_SETUP@CAP-XCR-E",
-    skip_reason="https://novantamotion.atlassian.net/browse/INGM-786",
-)
+@pytest.mark.repeat(100)
 def test_absolute_encoder_2_test(
     mc,
     alias,
