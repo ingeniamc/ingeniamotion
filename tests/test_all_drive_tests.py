de2aad4ed9b8e451d1bb6f9220ec70ae0cf82cbd
julieta.prieto <julieta.prieto@novanta.com>
2026-09-23 08:52:32 +0200

    INGM-784 remove repeat marker

--- tests/test_all_drive_tests.py
+++ tests/test_all_drive_tests.py
@@ -192,7 +192,6 @@ def test_incremental_encoder_1_test(
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.usefixtures("feedback_test_setup")
-@pytest.mark.repeat(100)
 def test_incremental_encoder_2_test(
     mc,
     alias,
