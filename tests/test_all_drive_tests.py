6760c0394ffd8c5dc2de4ab661b2f56db5ca081d
julieta.prieto <julieta.prieto@novanta.com>
2026-09-25 12:21:18 +0200

    INGM-787 remove repeat marker

--- tests/test_all_drive_tests.py
+++ tests/test_all_drive_tests.py
@@ -281,7 +281,6 @@ def test_absolute_encoder_2_test(
 @pytest.mark.soem
 @pytest.mark.canopen
 @pytest.mark.usefixtures("feedback_test_setup")
-@pytest.mark.repeat(100)
 def test_secondary_ssi_test(
     mc,
     alias,
